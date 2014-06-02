9e8c07eba182c778960296828bc1142fe78be5cd
Toni Lázaro <anlazes@gmail.com>
2014-06-02 19:20:08 +0200

    Diseño ventana principal

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class Principal extends JFrame {
@@ -30,12 +35,25 @@ public class Principal extends JFrame {
 	 * Create the frame.
 	 */
 	public Principal() {
+		setTitle("Inventario Multimedia");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 539, 376);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel imagen = new JLabel("New label");
+		imagen.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/multimedia-icons.jpg")));
+		imagen.setBounds(10, 0, 503, 194);
+		contentPane.add(imagen);
+		
+		JButton gestionBt = new JButton("Mi Biblioteca");
+		gestionBt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		gestionBt.setBounds(106, 236, 304, 44);
+		contentPane.add(gestionBt);
 	}
-
 }
