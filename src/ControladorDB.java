8d6cde29bb6c7da82c4c0c6544e4fd5c199a9458
Toni Lázaro <anlazes@gmail.com>
2014-06-03 17:43:50 +0200

    Añadidos métodos de consulta a DB para libros

--- src/ControladorDB.java
+++ src/ControladorDB.java
@@ -13,7 +13,7 @@ public class ControladorDB {
 	//Base de datos
 	Connection conexion = null; //maneja la conexión
 	Statement instruccion = null; //instrucción de consulta
-	ResultSet conjuntoResultados = null; //maneja los resultados
+	ResultSet resultados = null; //maneja los resultados
 		
 
 	public ControladorDB() {
@@ -33,7 +33,42 @@ public class ControladorDB {
 		catch( ClassNotFoundException noEncontroClase ) {
 			noEncontroClase.printStackTrace();
 		}
+	}
+	
+	//Método para consultar base de datos
+	public void leerLibros(JComboBox<String> listaLibros) {
+		try {		
+		//crea objeto Statement para consultar la base de datos	
+		instruccion = (Statement) conexion.createStatement();
+		//consulta la base de datos
+		resultados = instruccion.executeQuery("SELECT titulo FROM libros");		
+			while (resultados.next()) { 
+			this.nombre=(String)resultados.getObject("titulo");
+		
+			listaLibros.addItem(nombre);
+			}	
+			
+		} catch (SQLException slqex) {
+			slqex.printStackTrace();
+		}
 	}	
+	
+	//Método para insertar un nuevo libro en la base de datos
+	public void insertarLibro(String titulo, String autor, String genero, String editorial, JComboBox<String> listaLibros) {
+		//Aquí realizaremos la consulta
+		try {
+		//crea objeto Statement para consultar la base de datos	
+		instruccion = (Statement) conexion.createStatement();
+		String slq_ins="INSERT INTO libros(titulo, autor, genero, editorial)";
+		slq_ins=slq_ins+"VALUES('"+titulo+"','"+autor+"','"+genero+"','"+editorial+"')";
+		instruccion.executeUpdate(slq_ins);
+		//Actualización del combobox
+		listaLibros.removeAllItems();
+		leerLibros(listaLibros);
+		} catch (SQLException slqex) {
+			slqex.printStackTrace();
+		}
+	}
 		
 	public String toString() {
 		return nombre;
