1c2e6b62065988b7410034bfad8c7ae259536d4d
Toni Lázaro <anlazes@gmail.com>
2014-06-04 18:29:48 +0200

    Añade botón de consulta

--- src/Libros.java
+++ src/Libros.java
@@ -11,29 +11,42 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class Libros extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textoTitulo;
+	private	JTextField textoTitulo;
 	private JTextField textoAutor;
 	private JTextField textoGenero;
 	private JTextField textoEd;
+	private JButton guardarBtn;
+	private JButton eliminarBtn;
+	private JButton consultarBtn;
+	private String nombre;
 	private JComboBox<String> listaLibros;
 	
 	private ControladorDB conect=new ControladorDB();
-	private JButton guardarBtn;
-	private JButton eliminarBtn;
+	Connection conexion = null; //maneja la conexión
+	Statement instruccion = null; //instrucción de consulta
+	ResultSet resultados = null; //maneja los resultados
+
 
 	//Constructor de la clase Libros
 	public Libros() {
 		
 		iniciarVentana();
-		conect.leerLibros(listaLibros);
-	}	
+		conect.leerLibros(listaLibros);		
+	}
+	
 	
 	//Constructor de la ventana Libros
 	public void iniciarVentana() {
@@ -86,22 +99,76 @@ public class Libros extends JFrame {
 		contentPane.add(textoEd);
 		textoEd.setColumns(10);
 		
-		guardarBtn = new JButton("Guardar Libro");
+		guardarBtn = new JButton("Guardar");
 		guardarBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				//Llamada al método de insertar libros pasandole lo escrito en los campos de texto
-				conect.insertarLibro(textoTitulo.getText(), textoAutor.getText(), textoGenero.getText(), textoEd.getText(), listaLibros);			
+				conect.insertarLibro(textoTitulo.getText(), textoAutor.getText(), textoGenero.getText(), textoEd.getText(), listaLibros);	
+				listaLibros.getSelectedItem();
 			}
 		});
-		guardarBtn.setBounds(37, 248, 117, 23);
+		guardarBtn.setBounds(37, 248, 94, 23);
 		contentPane.add(guardarBtn);
 		
-		eliminarBtn = new JButton("Eliminar Libro");
+		eliminarBtn = new JButton("Eliminar");
 		eliminarBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		eliminarBtn.setBounds(270, 248, 126, 23);
+		eliminarBtn.setBounds(288, 248, 108, 23);
 		contentPane.add(eliminarBtn);
+		
+		consultarBtn = new JButton("Consultar");
+		consultarBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textoTitulo.setText("");
+				textoAutor.setText("");
+				textoGenero.setText("");
+				textoEd.setText("");
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					Connection conexion=DriverManager.getConnection("jdbc:mysql://localhost/biblioteca","root","");
+					Statement comando=conexion.createStatement();
+					ResultSet registro = comando.executeQuery("SELECT * FROM libros WHERE 1");
+					if (registro.next()==true) {
+						textoTitulo.setText(registro.getString("titulo"));
+						textoAutor.setText(registro.getString("autor"));
+						textoGenero.setText(registro.getString("genero"));
+						textoEd.setText(registro.getString("editorial"));
+					} else {
+						textoTitulo.setText("No hay registros");
+					}
+					conexion.close();
+				} catch(SQLException | ClassNotFoundException  ex){
+					setTitle(ex.toString());
+				}
+			}
+		});
+		consultarBtn.setBounds(164, 248, 89, 23);
+		contentPane.add(consultarBtn);
+	  	
+	}
+	
+	public void leerLibros(JComboBox<String> listaLibros) {
+		try {		
+		//crea objeto Statement para consultar la base de datos	
+		instruccion = (Statement) conexion.createStatement();
+		//consulta la base de datos
+		resultados = instruccion.executeQuery("SELECT titulo FROM libros");		
+			while (resultados.next()) { 
+			this.nombre=(String)resultados.getObject("titulo");
+		
+			listaLibros.addItem(nombre);
+			textoTitulo.setText(resultados.getString("titulo"));
+			}
+			
+		} catch (SQLException slqex) {
+			slqex.printStackTrace();
+		}
+	}
+	
+	public String toString() {
+		return nombre;
 	}
 }
+
